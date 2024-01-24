dfff4955eb6aff63c76d47c0bbd6057c51fc7258
AntoineJacob <5646@holbertonstudents.com>
2024-01-24 17:48:23 +0100

    Task 0

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Method for calcul the fewest number of operations needed
+"""
+
+
+def minOperations(n):
+    if (n < 1):
+        return 0
+
+    letters = 1
+    copy = 0
+    nbOpe = 0
+
+    while (letters != n):
+        if (n % letters == 0):
+            copy = letters
+            nbOpe += 1
+        letters += copy
+        nbOpe += 1
+
+    return nbOpe
